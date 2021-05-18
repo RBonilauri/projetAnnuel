141c1ca377086976cacfef16afe971941285d1cc
tbowetum <tbowetumawe@myges.fr>
2021-05-18 20:41:32 +0200

    teste

---
+++ teste.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bowet on 18/05/2021.
+//
+
+#include "teste.h"
---
+++ teste.h
@@ -0,0 +1,14 @@
+//
+// Created by bowet on 18/05/2021.
+//
+
+#ifndef PROJETANNUEL_TESTE_H
+#define PROJETANNUEL_TESTE_H
+
+
+class teste {
+
+};
+
+
+#endif //PROJETANNUEL_TESTE_H
