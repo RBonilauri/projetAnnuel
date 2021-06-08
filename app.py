45d28beaf11986a2119e0092867ee05b382d44ca
Rafael <rafael.bonilauri@wanadoo.fr>
2021-06-09 00:39:10 +0200

    python app

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import random
+
+def main():
+    st.markdown("")
+    st.title("PROJET ANNUEL")
+
+    st.text("The purpose of this application is to define the type of a given planet.\n"
+            "The answer can be 'telluric', 'gaseous' or 'other'.\n"
+            "\nTo do so, copy and paste the link of the desired image in the text bar below.\n")
+
+    #url = st.text_input("Paste image url")
+
+    form = st.form(key='planet_name')
+    url = form.text_input('Paste image url')
+    submit = form.form_submit_button('Submit')
+
+    if url != "":
+        st.image(url, width=400)
+        rand_int = random.Random()
+        st.title("Result :")
+        if rand_int == 1:
+            st.text("It's a telluric planet !")
+        else:
+            st.text("It's a gaseous planet !")
+
+
+if __name__ == "__main__":
+    main()
