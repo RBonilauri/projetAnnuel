e555db3c90ca361621922c2e12911129a1f540b0
Toky Cedric Andriamahefa <tokycedric2014@gmail.com>
2021-07-19 01:35:20 +0200

    Ajout dans l'app des moyens de choisir quel modele utiliser en fonction de inforamtions donné dans un json + modification du serveur pour avoir deux endpoint, l'un pour predict avec un MLP et l'autre pour predict avec un modele lineaire

--- ServeurML/ServeurFlask/app.py
+++ ServeurML/ServeurFlask/app.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 from entities import PMC
 import joblib
-from service.prediction import prediction_service, get_flat_image, get_prediction
+from service.prediction import prediction_service, get_flat_image, get_MLP_prediction, get_MLineaire_prediction
 
 app = Flask(__name__)
 
@@ -34,8 +34,8 @@ def testted():
     return response
 
 
-@app.route('/predict', methods=['GET'])
-def predict() :
+@app.route('/predictMLP', methods=['GET'])
+def predictMLP() :
     model_name = request.args.get("model")
     url = request.args.get("url")
 
@@ -47,7 +47,40 @@ def predict() :
 
     print(model_name)
 
-    prediction = get_prediction(model_name, image_for_test)
+    prediction = get_MLP_prediction(model_name, image_for_test)
+
+    print("model_name: ", model_name)
+    print("url: ", url)
+
+    data = {
+        "value" : json.dumps(str(prediction))
+    }
+
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+    print("data :", data)
+    print("response : ",response)
+
+    return response
+
+@app.route('/predictMLineaire', methods=['GET'])
+def predictMLineaire() :
+    model_name = request.args.get("model")
+    url = request.args.get("url")
+
+    model_name = model_name[1:len(model_name)-1]
+    url = url[1:len(url)-1]
+
+    im = Image.open(requests.get(url, stream=True).raw)
+    image_for_test = get_flat_image(im)
+
+    print(model_name)
+
+    prediction = get_MLineaire_prediction(model_name, image_for_test)
 
     print("model_name: ", model_name)
     print("url: ", url)
